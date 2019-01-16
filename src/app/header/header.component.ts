5d25d5fd92f0d22edde065c4263ae1f51f52dac2
MikelNazar <Mikel.nazar91@gmail.com>
2019-01-16 16:28:08 +0200

    first push

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,3 +14,5 @@ export class HeaderComponent implements OnInit {
   }
 
 }
+
+

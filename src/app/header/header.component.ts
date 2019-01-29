9ca3a9106270e8a3bd41f4abd8c2b9a7cacf2246
olehsul <oleh.sul@gmail.com>
2019-01-29 14:01:14 +0200

    Added login form to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TokenStorageService} from '../services/auth/token-storage.service';
+import {AuthService} from '../services/auth/auth.service';
+import {AuthLoginInfo} from '../services/auth/login-info';
+import {UserService} from '../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  roles: string[] = [];
+  private loginInfo: AuthLoginInfo;
+  isLoggedIn = false;
+  form: any = {};
 
-  constructor() { }
+
+  constructor(private tokenStorage: TokenStorageService,
+              private authService: AuthService,
+              private userService: UserService) {
+  }
+
+  // ngOnInit() {
+  //   this.isLoggedIn = !!!!!!!!!!!!!!!!this.tokenStorage.getToken();
+  //   if (this.tokenStorage.getToken()) {
+  //     this.isLoggedIn = true;
+  //     this.roles = this.tokenStorage.getAuthorities();
+  //   }
+  // }
 
   ngOnInit() {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenStorage.getAuthorities();
+    }
+  }
+
+  onSubmit() {
+    this.loginInfo = new AuthLoginInfo(
+      this.form.username,
+      this.form.password);
+
+
+    this.authService.attemptAuth(this.loginInfo).subscribe(
+      data => {
+        console.log(data);
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUsername(data.username);
+        this.tokenStorage.saveAuthorities(data.authorities);
+        this.goToUserPage(data.username);
+        this.roles = this.tokenStorage.getAuthorities();
+      }
+    );
+  }
+
+  goToUserPage(username: string) {
+    this.userService.getUserByUsername(username)
+      .subscribe(value => window.location.href = 'id/' + value.id, error => console.log(error));
   }
 
 }
